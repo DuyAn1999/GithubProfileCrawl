580643efdd0dc9dbfa74c42ca3250f9b88b20643
An Ngo <duy.an18041999@gmail.com>
2023-08-05 22:31:35 +0700

    Implement base logic

--- github-profile-crawl/app/page.tsx
+++ github-profile-crawl/app/page.tsx
@@ -1,112 +1,172 @@
+"use client";
 import Image from 'next/image'
+import { type } from 'os';
+import { useState, useEffect } from 'react';
+
+type GitHubProfileInfoType = GithubUserInfo & {
+  id: Number,
+  name: string,
+  url: string,
+  blog: string,
+  bio: string,
+  location: string,
+  public_repos: Number,
+}
+
+type GithubUserInfo = {
+  node_id: string,
+  avatar_url: string,
+  gravatar_id: string,
+  html_url: string,
+  followers_url: string,
+  following_url: string,
+  gists_url: string,
+  starred_url: string,
+  subscriptions_url: string,
+  organizations_url: string,
+  repos_url: string,
+  events_url: string,
+  received_events_url: string,
+  type: string,
+  site_admin: boolean
+}
+
+type RepoInfo = {
+  id: Number,
+  node_id: string,
+  name: string,
+  full_name: string,
+  private: boolean,
+  owner: GithubUserInfo,
+  html_url: string,
+  description: string,
+  fork: boolean,
+  url: string,
+  forks_url: string,
+  keys_url: string,
+  collaborators_url: string,
+  teams_url: string,
+  hooks_url: string,
+  issue_events_url: string,
+  events_url: string,
+  assignees_url: string,
+  branches_url: string,
+  tags_url: string,
+  blobs_url: string,
+  git_tags_url: string,
+  git_refs_url: string,
+  trees_url: string,
+  statuses_url: string,
+  languages_url: string,
+  stargazers_url: string,  
+  contributors_url: string,
+  subscribers_url: string,
+  subscription_url: string,
+  commits_url: string,
+  git_commits_url: string,
+  comments_url: string,
+  issue_comment_url: string,
+  contents_url: string,
+  compare_url: string,
+  merges_url: string,
+  archive_url: string,
+  downloads_url: string,
+  issues_url: string,
+  pulls_url: string,
+  milestones_url: string,
+  notifications_url: string,
+  labels_url: string,
+  releases_url: string,
+  deployments_url: string,
+  created_at: string,
+  updated_at: string,
+  pushed_at: string,
+  git_url: string,
+  ssh_url: string,
+  clone_url: string,
+  svn_url: string,
+  homepage: string,
+  size: Number,
+  stargazers_count: Number,
+  watchers_count: Number,
+  language: string,
+  has_issues: boolean,
+  has_projects: boolean,
+  has_downloads: boolean,
+  has_wiki: boolean,
+  has_pages: boolean,
+  has_discussions: boolean,
+  forks_count: Number,
+  mirror_url: string,
+  archived: boolean,
+  disabled: boolean,
+  open_issues_count: Number,
+  license: string,
+  allow_forking: boolean,
+  is_template: boolean,
+  web_commit_signoff_required: boolean,
+  topics: string[],
+  visibility: string,
+  forks: Number,
+  open_issues: Number,
+  watchers: Number,
+  default_branch: string
+}
 
 export default function Home() {
+  const [userName, setUserName] =  useState('');
+  const [profileInfo, setProfileInfo] =  useState<GitHubProfileInfoType>({});
+  const fetchUSerProfileInfo = async (userName: string) => {
+    try {
+      const res = await fetch(`https://api.github.com/users/${userName}`);
+      const data = res.json();
+      setProfileInfo(await data);
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
+
+  const fetchRepos = async () => {
+    try {
+      console.log(profileInfo)
+      const res = await fetch(profileInfo.repos_url);
+      const data = res.json();
+      console.log(await data);
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    fetchRepos();
+  }, [profileInfo])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className='container'>
+        <div className='search-container flex flex-col'>
+          <input className='search-input justify-center' value={userName} onChange={(e) => {setUserName(e.target.value)}}>
+          </input>
+          <button className='search-btn justify-center' onClick={async () => await fetchUSerProfileInfo(userName)}>Search</button>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {!profileInfo ||
+        (<div className='profileInfo'>
+          <img src={profileInfo.avatar_url}></img>
+          <div>
+            Profile name: {profileInfo.name}
+          </div>
+          <div>
+            Profile link: {profileInfo.url}
+          </div>
+          <div>
+            Repos: 
+            {}
+          </div>
+        </div>)
+}
       </div>
     </main>
   )
